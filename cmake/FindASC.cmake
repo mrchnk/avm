9e877902b2546755c1ead6010d78fbebb1680a47
mrchnk <mrchnk@gmail.com>
2021-02-17 13:00:24 +0300

    FindASC: remove flex sdk archive after asc extract

--- cmake/FindASC.cmake
+++ cmake/FindASC.cmake
@@ -8,16 +8,13 @@ set(FLEX_SDK_FILE ${CMAKE_BINARY_DIR}/apache-flex-sdk-4.16.1-bin.tar.gz)
 set(ASC_JAR ${CMAKE_BINARY_DIR}/asc.jar)
 
 if (NOT EXISTS ASC_JAR)
-    if (NOT EXISTS FLEX_SDK_FILE)
-        file(DOWNLOAD ${FLEX_SDK_URL} ${FLEX_SDK_FILE} EXPECTED_HASH MD5=${FLEX_SDK_MD5})
-    endif ()
-
+    file(DOWNLOAD ${FLEX_SDK_URL} ${FLEX_SDK_FILE} EXPECTED_HASH MD5=${FLEX_SDK_MD5})
     execute_process(
             COMMAND ${CMAKE_COMMAND} -E tar xzf ${FLEX_SDK_FILE} -- apache-flex-sdk-4.16.1-bin/lib/asc.jar
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-
     file(RENAME ${CMAKE_BINARY_DIR}/apache-flex-sdk-4.16.1-bin/lib/asc.jar ${ASC_JAR})
     file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/apache-flex-sdk-4.16.1-bin)
+    file(REMOVE ${FLEX_SDK_FILE})
 endif()
 
 set(ASC ${Java_JAVA_EXECUTABLE} -jar ${ASC_JAR})
